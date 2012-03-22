aff896660870c59c109f0c0b39c8ace36c80796c
Adria Cidre <adria.cidre@gmail.com>
2012-03-22 07:12:23 +0100

    Room:testConstructorForInvalidData

--- code/Room.php
+++ code/Room.php
@@ -11,17 +11,31 @@ class Room
     protected $_name;
     protected $_price;
     
+    /**
+     * Creates a new Room
+     * @param string $name
+     * @param float $price 
+     */
     public function __construct($name, $price)
     {
         $this->setName($name);
         $this->setPrice($price);
     }
     
+    /**
+     * Set the Room's name
+     * @param string $name 
+     */
     public function setName($name)
     {
         $this->_name = $name;
     }
     
+    
+    /**
+     * Set the Room's price 
+     * @param float $price 
+     */
     public function setPrice($price)
     {
         $this->_price = $price;
--- tests/unit/RoomTest.php
+++ tests/unit/RoomTest.php
@@ -25,6 +25,42 @@ class RoomTest extends PHPUnit_Framework_TestCase
         }
     }
     
+    /**
+     * Check if constructor works fine for invalid data
+     * @covers Room:__construct 
+     * @expectedException InvalidArgumentException
+     */
+    public function testConstructorForInvalidData()
+    {
+        $invalidDataPairs = array(
+            array(1, 'hola'),
+            array(array(), new stdClass()),
+            '1', '1.10'
+        );
+        
+        foreach ($invalidDataPairs as $data) {
+            $sw = 0;
+            try {
+                
+                $room = new Room($data[0], $data[1]);
+                $sw = 1;
+                
+            } catch(Exception $e) {
+                $this->assertTrue(true);
+            }
+            
+            if ($sw == 1) {
+                $this->fail(
+                    'Created new room with invalid data: '.
+                    var_export($data, true) . 
+                    ' and no exception throwed'
+                );
+            }
+        }
+    }
+    
+    
+    
     
     
 }
