a4a993799931b3e350f64f25fd046841cc228291
adriacidre <adria.cidre@gmail.com>
2013-02-12 23:52:20 +0100

    Travis config file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL);
 
-include_once("../code/Hotel.php");
-include_once("../code/Room.php");
-include_once("../code/Day.php");
+include_once(__DIR__ . "/../code/Hotel.php");
+include_once(__DIR__ . "/../code/Room.php");
+include_once(__DIR__ . "/../code/Day.php");
 
